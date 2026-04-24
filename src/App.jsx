a5a9e61a9fc45147d13165653f4e31c4aee4f254
Keith Owino <designsolutions1629@gmail.com>
2026-04-24 17:11:50 +0300

    Init build.

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+	return <section className="app">App</section>;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.htm", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
